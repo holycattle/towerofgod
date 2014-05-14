603a7160f7c1cc9834211cd4ed331ebfb37501f5
Joniel Ibasco <jlibasco@gmail.com>
2014-05-14 15:13:14 +0800

    Fixed camera.

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorController : MonoBehaviour {
+	public GameObject nextRoom;
+	public GameObject nextRoomDoor;
+	public bool isLocked;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void FixedUpdate () {
+		//if within the trigger and player presses interaction button, enter room associated with this
+
+	}
+
+	void OnTriggerStay2D(Collider2D other) {
+		Debug.Log("oops!");
+		if(other.transform.tag == "Player") {
+			if(Input.GetButtonUp("Interact")) {
+				other.transform.position = nextRoom.transform.FindChild("room_entrance").FindChild("door").position;
+				GameController.Instance.currentRoom = nextRoom;
+				MainCameraController.Instance.updateCamera();
+			}
+		}
+	}
+}
--- Assets/Scripts/MainCameraController.cs
+++ Assets/Scripts/MainCameraController.cs
@@ -5,45 +5,72 @@ public class MainCameraController : MonoBehaviour {
 	const float X_THRESHOLD = 1f;
 	const float Y_THRESHOLD = 1f;
 
-	const float MIN_X = 2.9f;
-	const float MAX_X = 10f;
+	//adjust depending on size of room
+	public float MIN_X;
+	//public float MAX_X = 4.4f;
+	public float MAX_X;
+	public float MIN_Y;
+	public float MAX_Y;
 
-	const float MIN_Y = 1.75f;
-	const float MAX_Y = 10f;
+	private static MainCameraController _instance;
 
 	float initialX;
 	float initialY;
 	float newX;
 	float newY;
+
 	GameObject player;
-	// Use this for initialization
+
+	void Awake() {
+		_instance = this;
+	}
+
 	void Start () {
 		player = GameObject.FindGameObjectWithTag("Player");
 		Debug.Log(player);
 		initialX = player.transform.position.x;
 		initialY = player.transform.position.y;
+
+		updateCamera();
+
+		Debug.Log(MIN_X);
 	}
-	
-	// Update is called once per frame
+
+	public void updateCamera() {
+		Room r = GameController.Instance.currentRoom.GetComponent<Room>();
+		Debug.Log("area " + r.name);
+		//min and max of camera are relative to the start and end points of each room
+		//MIN_X = GameController.Instance.currentRoom.transform.position.x + r.area.x/4.7f;
+		MIN_X = GameController.Instance.currentRoom.transform.position.x + 2.45f;
+		MAX_X = GameController.Instance.currentRoom.transform.position.x + 8.79f;
+		//MAX_X = GameController.Instance.currentRoom.transform.position.x + r.area.x/1.31f;
+		//MIN_Y = GameController.Instance.currentRoom.transform.position.y + r.area.y/2.9f;
+		MIN_Y = GameController.Instance.currentRoom.transform.position.y + 1.765f;
+		MAX_Y = GameController.Instance.currentRoom.transform.position.y + 3.01f;
+		//MAX_Y = GameController.Instance.currentRoom.transform.position.y + r.area.y/1.7f;
+
+		transform.position = new Vector3(MIN_X, MIN_Y, -10);
+		Debug.Log("new pos: " + transform.position);
+	}
+
 	void Update () {
 		float deltaX = player.transform.position.x - initialX;
 		float deltaY = player.transform.position.y - initialY;
-		Debug.Log(deltaX);
 		if(Mathf.Abs(deltaX) > X_THRESHOLD) {
-			Debug.Log("should be moving x");
 			newX = Mathf.Lerp(transform.position.x, player.transform.position.x, 8f * Time.deltaTime);
 		}
 		if(Mathf.Abs(deltaY) > Y_THRESHOLD) {
-			Debug.Log("should be moving x");
 			newY = Mathf.Lerp(transform.position.y, player.transform.position.y, 8f * Time.deltaTime);
 		}
 
+		//min and max should be relative to player's position and size of room
 		newX = Mathf.Clamp(newX, MIN_X, MAX_X);
 		newY = Mathf.Clamp(newY, MIN_Y, MAX_Y);
 
 		transform.position = new Vector3(newX, newY, transform.position.z);
 	}
-}
 
-//2.399814
-//1.758484
+	public static MainCameraController Instance {
+		get { return _instance; }
+	}
+}
