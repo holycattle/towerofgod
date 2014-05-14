865bae14c3188cd5fc6996a4fe6b3024e1ec056f
Joniel Ibasco <jlibasco@gmail.com>
2014-05-14 18:44:08 +0800

    HACK: fixed movement.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,13 +6,19 @@ using UnityEngine;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-	private const float MAX_SPEED = 1000f;
+	private const float MAX_SPEED = 2f;
 	private const float JUMPING_FORCE = 500f;
 	private const float MAX_JUMPING_FORCE = 20000f;
 	private const float MOVE_FORCE = 100f;
 	private bool facingRight;
 	private bool jumping;
-	
+
+	private static PlayerController _instance;
+
+	void Awake() {
+		_instance = this;
+	}
+
 	Animator animator;
 	// Use this for initialization
 	void Start() {
@@ -24,21 +30,17 @@ public class PlayerController : MonoBehaviour {
 	void Update() {
 		jumping = Input.GetButtonDown("Jump");
 	}
-
-	// Update is called once per frame
+	
 	void FixedUpdate() {
 		float direction = Input.GetAxis("Horizontal");
 		//Debug.Log(direction);
 		animator.SetFloat("Speed", Mathf.Abs(direction));
 
 		//if velocity hasn't maxed out yet, keep adding force
-		if(Mathf.Abs(direction) > 0 && rigidbody2D.velocity.x < MAX_SPEED) {
-			rigidbody2D.AddForce(Vector2.right * direction * MOVE_FORCE);
-		}
-
-		if(rigidbody2D.velocity.x >= MAX_SPEED) {
-			Debug.Log("maxed");
-			rigidbody2D.velocity = new Vector2(Mathf.Sign(direction) * MAX_SPEED, rigidbody2D.velocity.y);
+		if(Mathf.Abs(direction) > 0) {
+			//rigidbody2D.velocity = new Vector2(Mathf.Sign(direction) * MAX_SPEED, rigidbody2D.velocity.y);
+			transform.Translate(new Vector2(Time.fixedDeltaTime * 1.2f * direction, 0));
+			Debug.Log("should be moving: " + Mathf.Sign(direction) * MAX_SPEED * Time.deltaTime);
 		}
 
 		if(jumping) {
@@ -47,16 +49,24 @@ public class PlayerController : MonoBehaviour {
 			Debug.Log("Jump!");
 
 			//rigidbody2D.AddForce(new Vector2(rigidbody2D.velocity.x, JUMPING_FORCE));
-			rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, 35f);
+			rigidbody2D.velocity = new Vector2(direction * 12f, 35f);
 			Debug.Log(rigidbody2D.velocity);
 			/*if(rigidbody2D.velocity.y >= MAX_JUMPING_FORCE) {
 				jumping = false;
 			}*/
 		}
 
-		if (direction > 0 && !facingRight) {
+		if(rigidbody2D.velocity.y > 0) {
+			Debug.Log("should be ignoring platforms");
+			Debug.Log(LayerMask.NameToLayer("Platform"));
+			Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("Platform"), true);
+		} else {
+			Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("Platform"), false);
+		}
+
+		if(direction > 0 && !facingRight) {
 			Flip();
-		} else if (direction < 0 && facingRight) {
+		} else if(direction < 0 && facingRight) {
 			Flip();
 		}
 
@@ -68,5 +78,9 @@ public class PlayerController : MonoBehaviour {
 		newScale.x *= -1;
 		transform.localScale = newScale;
 		facingRight = !facingRight;
+	}
+
+	public static PlayerController Instance {
+		get { return _instance; }
 	}
 }
