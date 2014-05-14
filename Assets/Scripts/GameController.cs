fb082dd069f01fda3fdadd6ce4449af747a86626
Joniel Ibasco <jlibasco@gmail.com>
2014-05-14 16:49:59 +0800

    Room traversal.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	private int currentFloor = 1;
+	public GameObject currentRoom;
+	public bool isExitPresent = false;
+
+	private GameObject divergentRoom;
+	private GameObject entryRoom;
+	private GameObject exitRoom;
+	private GameObject fountainRoom;
+	private GameObject trapRoom;
+
+	private float currentX = 0f;
+	private float currentY = 0f;
+	
+	void Awake() {
+		// preload rooms
+		divergentRoom = (GameObject)Resources.Load("prefabs/rooms/DivergentRoom", typeof(GameObject)); // 7/20
+		entryRoom = (GameObject)Resources.Load("prefabs/rooms/EntryRoom", typeof(GameObject));
+		exitRoom = (GameObject)Resources.Load("prefabs/rooms/ExitRoom", typeof(GameObject)); //4/20
+		fountainRoom = (GameObject)Resources.Load("prefabs/rooms/FountainRoom", typeof(GameObject)); //1/20
+		trapRoom = (GameObject)Resources.Load("prefabs/rooms/TrapRoom", typeof(GameObject)); //8/20
+		//libraryRoom
+
+		_instance = this;
+	}
+	
+	// Use this for initialization
+	void Start () {
+		Debug.Log(entryRoom);
+		currentRoom = (GameObject)Instantiate(entryRoom, new Vector2(currentX, currentY), Quaternion.identity);
+		//currentRoom.name = "EntryRoom";
+		if(currentX >= 500f) {
+			currentX = 0f;
+			currentY += 100f;
+		} else {
+			currentX += 100f;
+		}
+		//Debug.Log(GameController.Instance.currentRoom.transform.FindChild("4-tile-piece"));
+	}
+	
+	public GameObject GenerateNewRoom(GameObject curr) {
+		string newName = "";
+		/*float p = Random.Range(0f, 1f);
+		while() {
+
+		}*/
+		GameObject n = curr;
+		while(n.name == curr.name) {
+			switch(Random.Range(0, 5)) {
+			case 0:
+				n = divergentRoom;
+				newName = "DivergentRoom";
+				break;
+			case 1:
+				n = exitRoom;
+				isExitPresent = true;
+				newName = "ExitRoom";
+				Debug.Log("exit is present!");
+				break;
+			/*case 2:
+				n = fountainRoom;
+				newName = "FountainRoom";
+				break;*/
+			case 2:
+				n = trapRoom;
+				newName = "TrapRoom";
+				break;
+			
+			case 3:
+				n = trapRoom;
+				newName = "TrapRoom";
+				break;
+			case 4:
+				n = divergentRoom;
+				newName = "DivergentRoom";
+				break;
+			case 5:
+				n = divergentRoom;
+				newName = "DivergentRoom";
+				break;
+			}
+		
+		}
+
+		GameObject newRoom = (GameObject)Instantiate(n, new Vector2(currentX, currentY), Quaternion.identity);
+		//newRoom.name = newName;
+
+		//update x, y
+		if(currentX >= 500f) {
+			currentX = 0f;
+			currentY += 100f;
+		} else {
+			currentX += 100f;
+		}
+
+		return newRoom;
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private static GameController _instance;
+
+	public static GameController Instance {
+		get { return _instance; }
+	}
+
+
+}
---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Room : MonoBehaviour {
+	private GameObject enemy;
+	private int enemyCount;
+
+	private static Dictionary<string, Vector2> roomAreas = new Dictionary<string, Vector2>();
+
+	//divergent room: 36x16
+	//entry room: 36x16
+	//exit room 24x16
+	//fountain room 20x16
+	//trap room 24x16
+	public Vector2 area;
+
+	void Awake() {
+		if(roomAreas.Count == 0) {
+			roomAreas.Add("DivergentRoom(Clone)", new Vector2(36*.32f, 16*.32f));
+			roomAreas.Add("EntryRoom(Clone)", new Vector2(36*.32f, 16*.32f));
+			roomAreas.Add("ExitRoom(Clone)", new Vector2(24*.32f, 16*.32f));
+			roomAreas.Add("FountainRoom(Clone)", new Vector2(20*.32f, 16*.32f));
+			roomAreas.Add("TrapRoom(Clone)", new Vector2(24*.32f, 16*.32f));
+		}
+		Debug.Log(transform.name);
+		area = roomAreas[transform.name];
+	}
+
+	// Use this for initialization
+	void Start () {
+		foreach(Transform t in transform) {
+			if(t.name == "room_exit") {
+				DoorController d = t.gameObject.GetComponentInChildren<DoorController>();
+				Debug.Log(d);
+				if(!GameController.Instance.isExitPresent) {
+					Debug.Log("creating!");
+					d.nextRoom = GameController.Instance.GenerateNewRoom(gameObject);
+					d.nextRoom.transform.FindChild("room_entrance").GetComponentInChildren<DoorController>().nextRoom = gameObject;
+					d.nextRoomDoor = d.nextRoom.transform.FindChild("room_entrance").FindChild("door").gameObject;
+
+					//point nextRoomEntrance to this room
+					GameObject nextRoomEntranceDoor = d.nextRoom.transform.FindChild("room_entrance").FindChild("door").gameObject;
+					DoorController dc = nextRoomEntranceDoor.GetComponent<DoorController>();
+					dc.nextRoom = gameObject;
+					dc.nextRoomDoor = transform.FindChild("room_exit").FindChild("door").gameObject;
+
+					Debug.Log(d);
+				} else {
+					Debug.Log("stop creating rooms");
+
+				}
+
+			} else if(t.name == "room_entrance") {
+
+			} else if(t.name == "level_exit") {
+				//clear scene
+				//reset params like isExitPresent
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
