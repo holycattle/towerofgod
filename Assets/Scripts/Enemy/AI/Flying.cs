4ef74aca409c4613f58f6e5512cc1069ab531a06
Joniel Ibasco <jlibasco@gmail.com>
2014-05-16 06:23:26 +0800

    Hacked up enemy AI.

---
+++ Assets/Scripts/Enemy/AI/Flying.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flying : MonoBehaviour {
+	public const float FLIP_COOLDOWN = 2.5f;
+	public const float AUTO_MOVE_RANGE = 1f;
+	//melee?
+	//TODO: set these to MELEE, RANGED, etc. constants later
+	public const float ATTACK_RANGE = 3f;
+	public const float SIGHT_RANGE = 3f;
+	
+	GameObject enemyObject;
+	
+	Transform target;
+	bool facingRight = true;
+	public int direction = 1;
+	float initialX;
+
+	public float moveSpeed;
+	
+	float flipCooldown = 0f;
+	
+	// Use this for initialization
+	void Start () {
+		//set this to root's transform
+		enemyObject = this.gameObject.transform.gameObject;
+		Debug.Log("eo: "+ enemyObject);
+		target = null;
+		initialX = enemyObject.transform.position.x;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		//Default behavior:
+		//periodically look left and right
+		//check if enemy is within range
+		//move towards enemy
+
+		if(Mathf.Abs(PlayerController.Instance.transform.position.x - transform.position.x) <= SIGHT_RANGE) {
+			target = PlayerController.Instance.transform;
+		} else {
+			target = null;
+		}
+
+		if(target == null) {
+			if(Mathf.Abs(initialX - enemyObject.transform.position.x) < AUTO_MOVE_RANGE) {
+				enemyObject.transform.Translate(new Vector2(Time.fixedDeltaTime * moveSpeed * direction, 0));
+			} else {
+				initialX = enemyObject.transform.position.x;
+				Flip();
+			}
+		} else {
+			if(enemyObject.transform.position.x > target.position.x && facingRight) {
+				Flip();
+			} else if(enemyObject.transform.position.x < target.position.x && !facingRight) {
+				Flip();
+			}
+
+			//if within range, attack
+			if(Mathf.Abs(target.position.x - enemyObject.transform.position.x) <= ATTACK_RANGE) {
+				enemyObject.GetComponent<Enemy>().Attack();
+				
+			}
+		}
+		
+	}
+	
+	/*void OnTriggerStay2D(Collider2D c) {
+		Debug.Log("player on sight! "+c.gameObject);
+		if(c.gameObject.transform.parent.name == "Player") {
+			Debug.Log("player on sight! "+c.gameObject);
+			target = c.gameObject.transform.parent;
+			Debug.Log("target: "+target.name);
+
+			if(enemyObject.transform.position.x > target.position.x && facingRight) {
+				Flip();
+			} else if(enemyObject.transform.position.x < target.position.x && !facingRight) {
+				Flip();
+			}
+		}
+		
+	}
+	
+	void OnTriggerExit2D(Collider2D c) {
+		Debug.Log("left! " + c.gameObject);
+		if(c.gameObject.transform.parent.name == "Player") {
+			target = null;
+		}
+	}*/
+	
+	void Flip() {
+		Vector3 newScale = enemyObject.transform.localScale;
+		newScale.x *= -1;
+		facingRight = !facingRight;
+		enemyObject.transform.localScale = newScale;
+		direction *= -1;
+	}
+
+}
--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -7,17 +7,78 @@ public class Enemy : MonoBehaviour {
 
 	public int spawnCost;
 
+	public float cooldown = 1f;
+	public float cooldownTimer = 0f;
+
 	//turn this into an enum later
 	public string type;
 
+	private GameObject fireballPrefab;
+
 	private Animator animator;
+
+	//TODO: refactor this to repeat repetition from PlayerControl
+	private GameObject damageCounter;
 
 	// Use this for initialization
 	void Start () {
+		damageCounter = (GameObject)Resources.Load("prefabs/UI/DamageCounter", typeof(GameObject));
 		animator = this.GetComponent<Animator>();
+		fireballPrefab = (GameObject)Resources.Load("prefabs/projectiles/fireball");
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		//later, check if object has a TakeDamage function
+		if(collision.gameObject.tag == "Player") {
+			PlayerController.Instance.TakeDamage(damage/2f);
+		}
+	}
+
+	void Update() {
+		if(health <= 0f) {
+			Destroy(gameObject);
+		}
 	}
 
 	public virtual void Attack() {
-		animator.SetTrigger("Attack");
+		//animator.SetTrigger("Attack");
+
+		//check if long range
+		if(cooldownTimer <= 0) {
+			//if melee, raycast and damage if it hits anything within a short range
+			cooldownTimer = cooldown;
+			Transform tmp = transform.FindChild("Projectiles");
+			if(gameObject.tag == "Melee") {
+				//.25f == melee range
+				RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.forward, 0.25f);
+				if(hit.collider.gameObject.name == "Player") {
+
+					Debug.Log("enemy attacked player!");
+					Debug.Break();
+					PlayerController.Instance.TakeDamage(damage);
+				}
+			} else if(gameObject.tag == "Ranged") {
+				//fire projectile
+				//HACK: since the flying guy, for now, is the only one with ranged capabilities, get Flying component
+				//TODO: make a superclass for Land and Flying and other types of AI to handle direction
+				Debug.Log("fire!!!!: "+transform.FindChild("Projectiles"));
+
+				GameObject g = (GameObject)Instantiate(fireballPrefab, new Vector3(tmp.position.x, tmp.position.y, 0), Quaternion.identity);
+				g.GetComponent<Spell>().direction = gameObject.GetComponent<Flying>().direction;
+				g.GetComponent<Spell>().damage = damage;
+				//ProjectilePool.Instance.GetFireball(transform.FindChild("Projectiles"), gameObject.GetComponent<Flying>().direction);
+				//Debug.Break();
+			}
+		} else {
+			cooldownTimer -= Time.deltaTime;
+		}
+	}
+
+	public virtual void TakeDamage(float d) {
+		health -= d;
+		GameObject damageCounterUI = (GameObject)Instantiate(damageCounter, Camera.main.WorldToViewportPoint(gameObject.transform.position), Quaternion.identity);
+		damageCounterUI.GetComponent<DamageCounter>().isDamage = true;
+		damageCounterUI.guiText.text = ""+ Mathf.Ceil(d);
+		Debug.Log("enemy h: "+health);
 	}
 }
