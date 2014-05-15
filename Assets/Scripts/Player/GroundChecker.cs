88b34d3352220063d1436b4c02d5cef430403ce4
Joniel Ibasco <jlibasco@gmail.com>
2014-05-16 06:31:34 +0800

    Fix infinite jumping issue.

---
+++ Assets/Scripts/Player/GroundChecker.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundChecker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//check if on the ground
+		RaycastHit2D raycasted = Physics2D.Raycast(transform.position, new Vector2(transform.up.x, transform.up.y*-1), 0.2f);
+		if(raycasted.collider != null) {
+			if(raycasted.collider.gameObject.layer == LayerMask.NameToLayer("Platform")) {
+				PlayerController.Instance.grounded = true;
+				PlayerController.Instance.timesJumped = 0;
+			}
+		}
+	}
+}
