772cf4e33de6ff52a0a07256187aebd83e4d3595
Joniel Ibasco <jlibasco@gmail.com>
2014-05-16 06:27:50 +0800

    Really basic UI for showing health and current floor.

---
+++ Assets/Scripts/GUI/PlayerStats.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStats : MonoBehaviour {
+
+	void OnGUI() {
+		//GUI.Box(new Rect(10,10,100,50), "Player");
+		GUILayout.BeginArea(new Rect(10,10,120,70), "Player", GUI.skin.window);
+		GUI.Label(new Rect(15,20,90,40), "Current floor: " + GameController.Instance.currentFloor);
+		GUI.Label(new Rect(15,40,90,40), "Health: " + PlayerController.Instance.health);
+		GUILayout.EndArea();
+	}
+}
