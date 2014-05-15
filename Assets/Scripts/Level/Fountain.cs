2c09d0083fd5135a3886efbe4a50d899d9337d1f
Joniel Ibasco <jlibasco@gmail.com>
2014-05-16 06:54:20 +0800

    Fixed fountain.

--- Assets/Scripts/Level/Fountain.cs
+++ Assets/Scripts/Level/Fountain.cs
@@ -10,14 +10,16 @@ public class Fountain : MonoBehaviour {
 		healCounter = (GameObject)Resources.Load("prefabs/UI/DamageCounter", typeof(GameObject));
 	}
 
-	void OnTriggerStay2D(Collider2D other) {
+	void OnTriggerEnter2D(Collider2D other) {
 		Debug.Log("inside fountain! "+other.gameObject.name);
 		if(other.gameObject.name == "Player") {
 			Debug.Log("should heal");
-			GameObject damageCounterUI = (GameObject)Instantiate(healCounter, Camera.main.WorldToViewportPoint(gameObject.transform.position), Quaternion.identity);
-			damageCounterUI.guiText.text = ""+ Mathf.Ceil(20f - PlayerController.Instance.health);
-			damageCounterUI.GetComponent<DamageCounter>().isDamage = false;
-			PlayerController.Instance.health = 20f;
+			if(PlayerController.Instance.health < 20f) {
+				GameObject damageCounterUI = (GameObject)Instantiate(healCounter, Camera.main.WorldToViewportPoint(gameObject.transform.position), Quaternion.identity);
+				damageCounterUI.guiText.text = ""+ Mathf.Ceil(20f - PlayerController.Instance.health);
+				damageCounterUI.GetComponent<DamageCounter>().isDamage = false;
+				PlayerController.Instance.health = 20f;
+			}
 		}
 	}
 }
